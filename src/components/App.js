c05a19b431c898a5bdc698b9cd3a2038772495e9
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-22 19:51:07 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
 
 import React from "react";
-import './../styles/App.css';
+//import './../styles/App.css';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const[datas,setDatas]=useState([]);
+  useEffect(()=>{
+      axios.get(' https://jsonplaceholder.typicode.com/posts')
+      .then(response=>{
+        setDatas(response.data)
+       console.log('the data',datas)
+      
+  })
+  .catch(error=>console.log(error))
+  }, [])
+
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+        {datas.map((value,index)=>(<div key={index}>
+            <h4>{`${value.id} .${value.title}`}</h4>
+            <p>{`${value.body}`}</p>
+          </div>
+        ))
+          
+        }
+       
     </div>
   )
 }
 
 export default App
+
+
